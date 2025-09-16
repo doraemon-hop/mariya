3a599c5051d5eab1480d314460078db7d6954dd1
BAGBAN FATEMA MO.AKIL <mariyabaghban@gmail.com>
2025-09-16 21:39:42 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,18 @@
+//program to findwhich number is bigger
+#include <stdio.h>
+void main() 
+{
+    int num1,num2;
+    
+    printf("\n enter the 2 number:");
+    scanf("%d%d",&num1,&num2);
+    
+    if(num1>num2)
+{
+    printf("\n number 1 is greater");
+}
+    if(num2>num1)
+    {
+        printf("\n number 2 is greater");
+    }
+}
