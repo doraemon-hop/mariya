d6ed3e68c2272684db75a765732d0ac6c60c870a
BAGBAN FATEMA MO.AKIL <mariyabaghban@gmail.com>
2025-09-16 23:21:57 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,11 @@
+// program to convert celcius to ferhenite
+#include <stdio.h>
+void main() 
+{  
+    float temp,c;
+     printf("\n enter temperature in celcius:");
+     scanf("%f",&temp);
+     c=(9/5)*(c+32);
+     printf("\n temperature in fahrenite is:%f",c);
+     
+}
