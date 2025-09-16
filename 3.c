f4e2dbc2e241c7e93ce310bc14e5b25b0bab6492
BAGBAN FATEMA MO.AKIL <mariyabaghban@gmail.com>
2025-09-16 21:31:37 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,17 @@
+program to verify if numbr is positive or not
+#include <stdio.h>
+void main() 
+{
+  int num;
+  printf("\n enter  number:");
+  scanf("\n %d",&num);
+  
+  if(num>0)
+{
+    printf("number is positive");
+} 
+  if(num<0)
+  {
+      printf("\n number is negetive:");
+  }
+}
