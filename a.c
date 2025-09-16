0f9ac583ec9e8742c7c081c6122af7114b03e227
doraemon-hop <mariyabaghban@gmail.com>
2025-09-16 19:28:53 +0530

    Create a.c

---
+++ a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int num1,num2,num3,sum=0;
+
+printf("entr 3 number:");
+scanf("%d%d%d",&num1&num2,&num3);
+
+sum=num1+num2+num3;
+printf("\n sum is:%d",sum);
+}
