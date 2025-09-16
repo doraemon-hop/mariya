2b1751fe0a3a0b518c5ee6b5f0134f2737093fe6
BAGBAN FATEMA MO.AKIL <mariyabaghban@gmail.com>
2025-09-16 21:15:55 +0530

    Create b.c

---
+++ b.c
@@ -0,0 +1,13 @@
+// program to calculate ara of rectangle
+#include <stdio.h>
+void main() 
+{
+   int  l,b,area;
+   printf(" enter leanth and breadth:");
+   scanf("%d%d",&l,&b);
+   
+   area=l*b;
+   
+   printf("\n area is:%d",area);
+   
+}
